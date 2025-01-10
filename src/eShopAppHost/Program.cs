c07223b9f5da5f35196c877211a828f2976babcb
Bruno Capuano <bcapuano@gmail.com>
2025-01-10 16:35:28 -0500

    Update OpenTelemetry and Azure Application Insights configurations

--- src/eShopAppHost/Program.cs
+++ src/eShopAppHost/Program.cs
@@ -1,35 +1,51 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var appInsights = builder.AddAzureApplicationInsights("appInsights");
+if (builder.ExecutionContext.IsPublishMode)
+{
+    var appInsights = builder.AddAzureApplicationInsights("appInsights");
+    var chatDeploymentName = "gpt-4o-mini";
+    var embeddingsDeploymentName = "text-embedding-ada-002";
+    var aoai = builder.AddAzureOpenAI("openai")
+        .AddDeployment(new AzureOpenAIDeployment(chatDeploymentName,
+        "gpt-4o-mini",
+        "2024-07-18",
+        "GlobalStandard",
+        10))
+        .AddDeployment(new AzureOpenAIDeployment(embeddingsDeploymentName,
+        "text-embedding-ada-002",
+        "2"));
 
-var chatDeploymentName = "gpt-4o-mini";
-var embeddingsDeploymentName = "text-embedding-ada-002";
-var aoai = builder.AddAzureOpenAI("openai")
-    .AddDeployment(new AzureOpenAIDeployment(chatDeploymentName,
-    "gpt-4o-mini",
-    "2024-07-18",
-    "GlobalStandard",
-    10))
-    .AddDeployment(new AzureOpenAIDeployment(embeddingsDeploymentName,
-    "text-embedding-ada-002",
-    "2"));
+    var sqldb = builder.AddSqlServer("sql")
+        .WithDataVolume()
+        .AddDatabase("sqldb");
 
-var sqldb = builder.AddSqlServer("sql")
-    .WithDataVolume()
-    .AddDatabase("sqldb");
+    var products = builder.AddProject<Projects.Products>("products")
+        .WithReference(sqldb)
+        .WaitFor(sqldb)
+        .WithReference(appInsights)
+        .WithReference(aoai)
+        .WithEnvironment("AI_ChatDeploymentName", chatDeploymentName)
+        .WithEnvironment("AI_embeddingsDeploymentName", embeddingsDeploymentName);
 
-var products = builder.AddProject<Projects.Products>("products")
-    .WithReference(sqldb)
-    .WaitFor(sqldb)
-    .WithReference(appInsights)
-    .WithReference(aoai)
-    .WithEnvironment("AI_ChatDeploymentName", chatDeploymentName)
-    .WithEnvironment("AI_embeddingsDeploymentName", embeddingsDeploymentName);
+    var store = builder.AddProject<Projects.Store>("store")
+        .WithReference(products)
+        .WaitFor(products)
+        .WithReference(appInsights)
+        .WithExternalHttpEndpoints();
+}
+else
+{
+    var sqldb = builder.AddSqlServer("sql")
+        .WithDataVolume()
+        .AddDatabase("sqldb");
 
-var store = builder.AddProject<Projects.Store>("store")
-    .WithReference(products)
-    .WaitFor(products)
-    .WithReference(appInsights)
-    .WithExternalHttpEndpoints();
+    var products = builder.AddProject<Projects.Products>("products")
+        .WithReference(sqldb)
+        .WaitFor(sqldb);
 
-builder.Build().Run();
+    var store = builder.AddProject<Projects.Store>("store")
+        .WithReference(products)
+        .WaitFor(products)
+        .WithExternalHttpEndpoints();
+}
+    builder.Build().Run();
