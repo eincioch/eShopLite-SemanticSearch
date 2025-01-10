5315809a31d9c9aaa221e83311e9be8fe817932b
Bruno Capuano <bcapuano@gmail.com>
2025-01-10 17:15:36 -0500

    Refactor Program.cs to streamline database and project initialization for production mode

--- src/eShopAppHost/Program.cs
+++ src/eShopAppHost/Program.cs
@@ -1,7 +1,21 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var sqldb = builder.AddSqlServer("sql")
+    .WithDataVolume()
+    .AddDatabase("sqldb");
+
+var products = builder.AddProject<Projects.Products>("products")
+    .WithReference(sqldb)
+    .WaitFor(sqldb);
+
+var store = builder.AddProject<Projects.Store>("store")
+    .WithReference(products)
+    .WaitFor(products)
+    .WithExternalHttpEndpoints();
+
 if (builder.ExecutionContext.IsPublishMode)
 {
+    // production code uses Azure services, so we need to add them here
     var appInsights = builder.AddAzureApplicationInsights("appInsights");
     var chatDeploymentName = "gpt-4o-mini";
     var embeddingsDeploymentName = "text-embedding-ada-002";
@@ -15,37 +29,13 @@ if (builder.ExecutionContext.IsPublishMode)
         "text-embedding-ada-002",
         "2"));
 
-    var sqldb = builder.AddSqlServer("sql")
-        .WithDataVolume()
-        .AddDatabase("sqldb");
-
-    var products = builder.AddProject<Projects.Products>("products")
-        .WithReference(sqldb)
-        .WaitFor(sqldb)
-        .WithReference(appInsights)
+    products.WithReference(appInsights)
         .WithReference(aoai)
         .WithEnvironment("AI_ChatDeploymentName", chatDeploymentName)
         .WithEnvironment("AI_embeddingsDeploymentName", embeddingsDeploymentName);
 
-    var store = builder.AddProject<Projects.Store>("store")
-        .WithReference(products)
-        .WaitFor(products)
-        .WithReference(appInsights)
+    store.WithReference(appInsights)
         .WithExternalHttpEndpoints();
 }
-else
-{
-    var sqldb = builder.AddSqlServer("sql")
-        .WithDataVolume()
-        .AddDatabase("sqldb");
 
-    var products = builder.AddProject<Projects.Products>("products")
-        .WithReference(sqldb)
-        .WaitFor(sqldb);
-
-    var store = builder.AddProject<Projects.Store>("store")
-        .WithReference(products)
-        .WaitFor(products)
-        .WithExternalHttpEndpoints();
-}
-    builder.Build().Run();
+builder.Build().Run();
